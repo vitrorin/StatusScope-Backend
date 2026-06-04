26704073fada3555716a19120c61d7054d98d4bf
Edgar Osorio <edgar.osorio@bwr.mx>
2026-06-03 18:20:44 -0600

    Pipeline
    Se implento cambios para implementar el pipeline

--- src/main/java/com/itesm/application/port/out/OperationalEmailMessage.java
+++ src/main/java/com/itesm/application/port/out/OperationalEmailMessage.java
@@ -1,3 +1,8 @@
 package com.itesm.application.port.out;
 
+<<<<<<< Updated upstream
 public record OperationalEmailMessage(String to, String subject, String body) {}
+=======
+public record OperationalEmailMessage(String to, String subject, String body) {
+}
+>>>>>>> Stashed changes
